29e7a186dab0164c258d28f6df9f28f67730dd6a
JKSpectator <spectatorjk@gmail.com>
2025-11-21 22:00:38 +0800

    update readme

---
+++ pointcept/models/cylinderformer/__init__.py
@@ -0,0 +1,3 @@
+from .cylinderformer import *
+from .cylinderformer_teacher import *
+from .cylinderformer_student import *
--- pointcept/models/point_transformer_v3/__init__.py
+++ pointcept/models/point_transformer_v3/__init__.py
@@ -1,4 +1,1 @@
 from .point_transformer_v3m1_base import *
-from .cylinderformer import *
-from .cylinderformer_teacher import *
-from .cylinderformer_student import *
